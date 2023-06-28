6d686f8df7582bf50675dbdb2c3209b5ba09a2d0
rumatt <96069186+7yhx@users.noreply.github.com>
2023-06-28 20:10:04 +0300

    Update hub loader.lua

--- hub loader.lua
+++ hub loader.lua
@@ -1,7 +1,7 @@
 repeat wait() until game:IsLoaded()
 local PlaceId = game.PlaceId
 if PlaceId == 6361937392 then
-loadstring(game:HttpGet("https://raw.githubusercontent.com/7yhx/Valyrift_Hub/main/steal%20time%20from%20others")
+loadstring(game:HttpGet("https://raw.githubusercontent.com/7yhx/Valyrift_Hub/main/stfo.lua")
   else
     local notification = loadstring(game:HttpGet("https://raw.githubusercontent.com/AbstractPoo/Main/main/Notifications.lua"))()
     notification:notify{
