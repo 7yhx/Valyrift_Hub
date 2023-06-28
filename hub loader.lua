95d45a968e9599d38140c0e982a223dee954af08
rumatt <96069186+7yhx@users.noreply.github.com>
2023-06-28 18:08:52 +0300

    Update hub loader.lua

--- hub loader.lua
+++ hub loader.lua
@@ -2,4 +2,11 @@ repeat wait() until game:IsLoaded()
 local PlaceId = game.PlaceId
 if PlaceId == 6361937392 then
 loadstring(game:HttpGet("https://raw.githubusercontent.com/7yhx/Valyrift_Hub/main/steal%20time%20from%20others")
-end
+  else
+    local notification = loadstring(game:HttpGet("https://raw.githubusercontent.com/AbstractPoo/Main/main/Notifications.lua"))()
+    notification:notify{
+        Title = "Game Unsupported",
+        Description = "Sorry, this game is unsupported.",
+        Length = 10
+    }
+  end
