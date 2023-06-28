b2781f157b63d5c5ff1d26da19554d7713778b1b
rumatt <96069186+7yhx@users.noreply.github.com>
2023-06-28 18:06:29 +0300

    Update hub loader.lua

--- hub loader.lua
+++ hub loader.lua
@@ -1,1 +1,5 @@
-
+repeat wait() until game:IsLoaded()
+local PlaceId = game.PlaceId
+if PlaceId == 6361937392 then
+loadstring(game:HttpGet("https://raw.githubusercontent.com/7yhx/Valyrift_Hub/main/steal%20time%20from%20others")
+end
