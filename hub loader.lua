083f5a2ab824c5f8335f30dcaf3376600fdd9638
rumatt <96069186+7yhx@users.noreply.github.com>
2023-07-09 12:56:19 +0300

    Update hub loader.lua

--- hub loader.lua
+++ hub loader.lua
@@ -1,7 +1,7 @@
 repeat wait() until game:IsLoaded()
 local PlaceId = game.PlaceId
 if PlaceId == 6361937392 then
-loadstring(game:HttpGet("https://raw.githubusercontent.com/7yhx/Valyrift_Hub/main/stfo.lua")
+loadstring(game:HttpGet("https://raw.githubusercontent.com/7yhx/Valyrift_Hub/main/stfo.lua"))()
   else
 local Player = game.Players.LocalPlayer
 Player:Kick("Game Unsupported.")
